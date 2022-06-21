6e971a7829ea41c6b1d4d693683baca5fb4b4ed8
tim mckiernan <tim@pursuit.org>
2022-06-21 16:55:25 -0400

    adds money

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>To do: hire people to make youtube clone </h1>
+      <h1>To do: hire people to make youtube clone  && make money</h1>
 
     </div>
   );
